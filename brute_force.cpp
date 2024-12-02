aa0b47920bb95e39130385a8fdb0f59eef224e58
Md Zunayed <120125367+mdzunayed@users.noreply.github.com>
2024-12-02 21:04:56 +0600

    Update brute_force.cpp

--- brute_force.cpp
+++ brute_force.cpp
@@ -2,19 +2,6 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-#define faster ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define all(a) (a).begin(),(a).end()
-#define nl cout<<'\n' ;
-#define dbg(x) cerr<<"Line no- "<<__LINE__<<": "<<#x<<" = "<<x<<'\n';
-#define out(a) cout<<a<<'\n'; 
-#define hmm(ok) cout<<(ok==1?"YES":"NO")<<'\n';
-#define ld long double
-#define ll long long
-#define ull unsigned long long
-const ld PI = 3.141592653589793238462;
-const ll MOD = 1e9+7;
-const ll INF = 1e18;
-
 void solve() {
     int n;
     cin>>n;
